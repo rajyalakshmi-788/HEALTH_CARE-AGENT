180e8a684f8d961f655a33e0392b884c282df309
rajyalakshmi-788 <rajyalakshmigunji26@gmail.com>
2026-06-03 09:20:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -101,5 +101,6 @@ import os
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
+        port=int(os.environ.get("PORT", 10000)),
+        debug=False
     )
