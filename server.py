efe84129e7f06372f5843b7efc43c86c60335f98
rajyalakshmi-788 <rajyalakshmigunji26@gmail.com>
2026-06-03 09:03:27 +0530

    Modify server to use environment variable for port
    Updated server configuration to run on environment-defined port.

--- server.py
+++ server.py
@@ -97,4 +97,10 @@ def process_agentic_swarm():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
